6db4fcba3d6a1073f63819c8ff210d42b934d2b4
Nebir Kiron <kironnibir046@gmail.com>
2021-04-28 02:28:31 +0600

    order model created

--- Courier/admin.py
+++ Courier/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Marchant,Product,Order
 # Register your models here.
+
+admin.site.register(Marchant)
+admin.site.register(Product)
+admin.site.register(Order)
--- Courier/models.py
+++ Courier/models.py
@@ -7,6 +7,28 @@ class Marchant(models.Model):
     phone = models.CharField(max_length=50,null=True)
     address = models.CharField(max_length=500,null=True)
     email = models.EmailField(max_length=254,null=True)
-    create_date = models.DateTimeField( auto_now_add=False,null=True)
+    create_date = models.DateTimeField( auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.category
+    
 
 
+class Order(models.Model):
+    marchant = models.ForeignKey(Marchant, null=True, on_delete=models.SET_NULL)
+    product_category = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL) 
+    order_Area = models.CharField(max_length=200, null=True )
+    date_created = models.DateTimeField( auto_now_add=True,null=True)
+    order_status = models.CharField(max_length=200, null=True )
+    order_addres  =  models.CharField(max_length=500, null=True )
+    order_quantity = models.FloatField()
+    order_price = models.DecimalField(max_digits=10, decimal_places=2)
+ 
